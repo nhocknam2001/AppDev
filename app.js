0a0b2625e8640289abc9e75c18ff937c9d064551
DoQuocBinh <binhdq@fpt.edu.vn>
2022-03-22 10:18:30 +0700

    login- session

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 
+const session = require('express-session')
+app.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 60000 }, saveUninitialized: false, resave: false }))
+
 //su dung HBS: =>res.render('....')
 app.set('view engine', 'hbs')
 //lay du lieu tu cac Form: textbox, combobox...
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/',(req,res)=>{
-    res.render('home')
+    res.render('home',{userInfo:req.session.User})
+    //res.render('home')
 })
 
 const adminController = require('./controllers/admin')
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,35 @@
 const express = require('express')
+const async = require('hbs/lib/async')
 const router = express.Router()
-const {insertObject} = require('../databaseHandler')
+const {insertObject,checkUserRole,USER_TABLE_NAME} = require('../databaseHandler')
 
 //neu request la: /admin/register
 router.get('/register',(req,res)=>{
     res.render('register')
+})
+
+//kiem tra thong tin login
+router.post('/login',async (req,res)=>{
+    const name = req.body.txtName
+    const pass= req.body.txtPassword
+    const role =await checkUserRole(name,pass)
+    if (role=="-1"){
+        res.render('login')
+        return
+    }else{
+        console.log("You are a/an: " +role)
+        req.session["User"] = {
+            'userName': name,
+            'role': role //admin hoac staff
+        }
+        //res.render('home',{userInfo:req.session.User})
+        res.redirect('/')
+    }
+
+})
+
+router.get('/login',(req,res)=>{
+    res.render('login')
 })
 
 router.post('/register',(req,res)=>{
@@ -18,7 +43,7 @@ router.post('/register',(req,res)=>{
         password: pass
     }
     //goi ham insert: bang Users, new user trong objectToInsert
-    insertObject("Users",objectToInsert)
+    insertObject(USER_TABLE_NAME,objectToInsert)
     res.render('home')
 })
 
