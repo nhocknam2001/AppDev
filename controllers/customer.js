d7a14a47a43cb31f86b075a8a0877a4e95b18be7
Thuy Duong <dangthuyduong12a4@gmail.com>
2022-04-07 09:07:28 +0700

    74-2

--- controllers/customer.js
+++ controllers/customer.js
@@ -5,6 +5,33 @@ router.get('/addCart',(req,res)=>{
     const id = req.query.id
     //lay bien cart trong session co the chua co gia tri hoac co gia tri
     let myCart = req.session["Cart"]
+    if(myCart == null){ //day la lan dau tien add sp vao Cart
+        var dict = {}
+        dict[id] = 1
+        console.log('Ban da mua san pham dau tien:' + id)
+        req.session["Cart"]=dict
+    } else{//da mua it nhat 1 san pham
+        var dict = req.session["Cart"]
+        var oldProduct = dict[id]
+        //kiem tra san pham dang mua torng cart chua
+        if(oldProduct == null)
+            dict[id] = 1 //chua co: cho so luong = 1
+        else{
+            const oldQuantity = parseInt(oldProduct) //lay so luong cu 
+            dict[id] = oldProduct + 1 //da co: so luong tang them 1
+        }
+        //cap nhat gio hang bang tu dien
+        req.session["cart"] = dict
+    }
+
+    let spDamua = []
+    //neu khach hang da mua it nhat 1 san pham 
+    const dict2 = req.session["cart"]
+    for (var key in dict2) {
+        spDamua.push({masp:key, soLuong: dict2[key]})
+    }
+    res.render('myCart',{'cart':spDamua})
+
 })
 
 //global scope
