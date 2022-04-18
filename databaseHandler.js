8a6552123aeb02e90709e6fb716ac3fbbc3898ad
TrungDung03 <dungdtgch190880@fpt.edu.vn>
2022-04-18 23:57:15 +0700

    update

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,36 +1,43 @@
-const async = require('hbs/lib/async');
 const {MongoClient,ObjectId} = require('mongodb');
 
 const DATABASE_URL = 'mongodb+srv://quyennxgch190732:quyen692001@cluster0.76ku8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const DATABASE_NAME = 'ApplicationDev';
 
-async function getDB() {
-    const client = await MongoClient.connect(URL);
+async function getDatabase() {
+    const client = await MongoClient.connect(DATABASE_URL);
     const dbo = client.db(DATABASE_NAME);
     return dbo;
 }
 
-async function insertObject(collectionName,objectToInsert){
-    const dbo = await getDB();
-    const newObject = await dbo.collection(collectionName).insertOne(objectToInsert);
-    console.log("Gia tri id moi duoc insert la: ", newObject.insertedId.toHexString());
+async function deleteProduct(collectionName, id){
+    const dbo = await getDatabase()
+    await dbo.collection(collectionName).deleteOne({_id: ObjectId(id)})
 }
 
-async function  checkUserRole(nameI,passI){
-    const dbo = await getDB();
-
-    const user= await dbo.collection(USER_TABLE_NAME).findOne({userName:nameI,password:passI});
-    //Neu khong trung user name va password
-    if (user==null) {
-        return "-1"
-    }else{
-        console.log(user)
-        //tra lai: role cua user: admin hoac staff
-        return user.role;
-    }
+async function getAllDocumentsFromCollection(collectionName) {
+    const dbo = await getDatabase();
+    const products = await dbo.collection(collectionName).find({}).toArray();
+    return products;
 }
-//
 
-const USER_TABLE_NAME = "Users"
+async function getDocumentById(collectionName, id) {
+    
+    const dbo = await getDatabase();
+    const productToEdit = await dbo.collection(collectionName).findOne({ _id: ObjectId(id) });
+    return productToEdit;
+}
 
-module.exports = {insertObject,checkUserRole,USER_TABLE_NAME}
+async function insertObjectToCollection(collectionName, newP) {
+    const dbo = await getDatabase();
+    const result = await dbo.collection(collectionName).insertOne(newP);
+    console.log("The newly user inserted id value is: ", result.insertedId.toHexString());
+}
+
+async function updateCollection(id, collectionName, newvalues) {
+    const myquery = { _id: ObjectId(id) };
+    const dbo = await getDatabase();
+    await dbo.collection(collectionName).updateOne(myquery, newvalues);
+}
+
+module.exports = {getDatabase,deleteProduct, getAllDocumentsFromCollection,
+    getDocumentById, insertObjectToCollection, updateCollection}
